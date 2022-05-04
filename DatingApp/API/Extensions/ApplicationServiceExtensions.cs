03f04c2c0bab21c0f43c4022148da60136f93d46
Darya Norko <d.i.norko@mail.ru>
2022-05-04 14:10:01 +0300

    End of section 5

--- DatingApp/API/Extensions/ApplicationServiceExtensions.cs
+++ DatingApp/API/Extensions/ApplicationServiceExtensions.cs
@@ -19,7 +19,7 @@ namespace API.Extensions
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
 
-            return services;
+            return services; 
         }
     }
 }
