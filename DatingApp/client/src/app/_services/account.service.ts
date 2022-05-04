5426baf59a1ead8d8432912fdb4fbd62666d16d0
Darya Norko <d.i.norko@mail.ru>
2022-05-04 22:10:38 +0300

    End of section 6

--- DatingApp/client/src/app/_services/account.service.ts
+++ DatingApp/client/src/app/_services/account.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { User } from '../_models/user';
 
 @Injectable({
@@ -9,17 +9,18 @@ import { User } from '../_models/user';
 })
 export class AccountService {
   baseUrl = 'https://localhost:5001/api/';
-  private currentUserSource = new ReplaySubject<User>(1);
-  currentUser$ = this.currentUserSource.asObservable();
-
+  private currentUserSource = new ReplaySubject<User>(1); // мы обещаем, что туда когда-нибудь попадёт 1 объект user
+  currentUser$ = this.currentUserSource.asObservable(); // асинхронность для js. мы ждем ответа от сервака, поэтому асинхронно
 
   constructor(private http: HttpClient) { }
+  // даёт обертку чтобы обращаться к серверу
 
   login(model: any) {
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
-      map((response: User) => {
+      // в него прописываем нес-ко функций, и когда придёт ответ с сервера, мы прогоняем его по этим функциям
+      map((response: User) => { // преобразовывает каждый элемент как мне надо
         const user = response;
-        if(user) {
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource.next(user);
         }
@@ -27,12 +28,14 @@ export class AccountService {
     );
   }
 
-  register(model:any){
+  register(model: any) {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
-        if (user){
+        if (user) {
           localStorage.setItem('user', JSON.stringify(user));
+          // localStorage - локальное хранилище в браузере. туда засовывается юзер
           this.currentUserSource.next(user);
+          // c пом. метода next мы засовываем в currentUserSource user
         }
       })
     )
@@ -41,10 +44,12 @@ export class AccountService {
 
   setCurrentUser(user: User) {
     this.currentUserSource.next(user);
-  }
+  } //перезапись юзера
 
-  logout(){
+  logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+    // чистим, чтобы были одинковые данные (то есть null везде)
   }
 }
+ 
