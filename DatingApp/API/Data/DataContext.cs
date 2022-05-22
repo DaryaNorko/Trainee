e2abd78245b6d13e11de9f89ae157a729b414676
Darya Norko <d.i.norko@mail.ru>
2022-05-22 21:03:53 +0300

    End of section 18

--- DatingApp/API/Data/DataContext.cs
+++ DatingApp/API/Data/DataContext.cs
@@ -6,11 +6,15 @@ using Microsoft.EntityFrameworkCore;
 using API.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using API.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace API.Data
 {
     public class DataContext : IdentityDbContext<AppUser, AppRole, int,
-        IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>, 
+        IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>,
         IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public DataContext(DbContextOptions options) : base(options)
@@ -39,7 +43,7 @@ namespace API.Data
 
 
             builder.Entity<UserLike>()
-            .HasKey(k => new {k.SourceUserId, k.LikedUserId});
+            .HasKey(k => new { k.SourceUserId, k.LikedUserId });
 
             builder.Entity<UserLike>()
                 .HasOne(s => s.SourceUser)
@@ -53,15 +57,46 @@ namespace API.Data
            .HasForeignKey(s => s.LikedUserId) // внешний ключ к таблице
            .OnDelete(DeleteBehavior.Cascade);
 
-           builder.Entity<Message>()
-            .HasOne(u => u.Recipient)
-            .WithMany(m => m.MessagesReceived)
-            .OnDelete(DeleteBehavior.Restrict);
+            builder.Entity<Message>()
+             .HasOne(u => u.Recipient)
+             .WithMany(m => m.MessagesReceived)
+             .OnDelete(DeleteBehavior.Restrict);
 
             builder.Entity<Message>()
             .HasOne(u => u.Sender)
             .WithMany(m => m.MessagesSent)
             .OnDelete(DeleteBehavior.Restrict);
+
+            builder.ApplyUtcDateTimeConverter();
+        }
+    }
+
+    public static class UtcDateAnnotation
+    {
+        private const String IsUtcAnnotation = "IsUtc";
+        private static readonly ValueConverter<DateTime, DateTime> UtcConverter =
+            new ValueConverter<DateTime, DateTime>(v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+        private static readonly ValueConverter<DateTime?, DateTime?> UtcNullableConverter =
+            new ValueConverter<DateTime?, DateTime?>(v => v, v => v == null ? v : DateTime.SpecifyKind(v.Value, DateTimeKind.Utc));
+
+        public static PropertyBuilder<TProperty> IsUtc<TProperty>(this PropertyBuilder<TProperty> builder, Boolean isUtc = true) =>
+            builder.HasAnnotation(IsUtcAnnotation, isUtc);
+
+        public static Boolean IsUtc(this IMutableProperty property) =>
+            ((Boolean?)property.FindAnnotation(IsUtcAnnotation)?.Value) ?? true;
+
+        public static void ApplyUtcDateTimeConverter(this ModelBuilder builder)
+        {
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (!property.IsUtc()) continue;
+                    if (property.ClrType == typeof(DateTime)) property.SetValueConverter(UtcConverter);
+                    if (property.ClrType == typeof(DateTime?)) property.SetValueConverter(UtcNullableConverter);
+                }
+            }
         }
     }
 }
--- DatingApp/API/Helpers/AutoMapperProfiles.cs
+++ DatingApp/API/Helpers/AutoMapperProfiles.cs
@@ -25,7 +25,6 @@ namespace API.Helpers
                     src.Sender.Photos.FirstOrDefault(x => x.IsMain).Url))
                 .ForMember(dest => dest.RecipientPhotoUrl, memberOptions => memberOptions.MapFrom(src =>
                     src.Recipient.Photos.FirstOrDefault(x => x.IsMain).Url));
-            CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
         }
     }
 }
